ec1a07f3cc10e6f0c71182525a5795ecfb982986
Andy <siwaa@mail.ru>
2022-09-28 11:28:27 +0300

    add: error page handler

---
+++ layouts/error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="error container">
+    <h1 v-if="error.statusCode === 404">Страница не найдена</h1>
+    <h1 v-else>Хм.. Что-то пошло не так</h1>
+    <br>
+    <NuxtLink to="/">Вернуться на главную</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+}
+</script>
+
+<style lang="postcss">
+.error {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
